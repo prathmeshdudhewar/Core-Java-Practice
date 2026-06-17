8f20f5ea9ea1124e555fbdc01c91349c08cc15d3
Prathmesh Dudhewar <prathmeshdudhewar34@gmail.com>
2026-06-17 08:38:04 +0530

    Added Core Java programs

---
+++ VariableDemo.java
@@ -0,0 +1,15 @@
+// Class declaration
+class VariableDemo 
+{
+
+    // Main method - Program execution starts from here
+    public static void main(String[] args) 
+	{
+
+        // Variable declaration and initialization
+        int age = 21;
+
+        // Printing the value of age variable
+        System.out.println("Age = " + age);
+    }
+}
